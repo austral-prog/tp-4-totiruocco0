590e5897801858652acee5fad3a22bfb266b79d6
totiruocco0 <162723302+totiruocco0@users.noreply.github.com>
2024-04-17 21:37:30 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,13 @@
 def leap_year():
-    print("TO DO")
+    Y = input("Ingrese un año: ")
+    Y = int(Y)
+    Y4 = Y % 4
+    y100 = Y % 100
+    Y400 = Y % 400
+    if Y400 == 0:
+        print(f'El año {Y} es bisiesto')
+    elif Y4 == 0 and y100 != 0 :
+        print(f'El año {Y} es bisiesto')
+    else:
+        print(f'El año {Y} no es bisiesto')
+leap_year()
